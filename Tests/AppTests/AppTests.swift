293f534a3b96a68c04b3a0106c2ca6f3cc440ad1
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2024-03-20 14:16:02 -0300

    creating remote repository

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -6,10 +6,28 @@ final class AppTests: XCTestCase {
         let app = Application(.testing)
         defer { app.shutdown() }
         try await configure(app)
-
+        
         try app.test(.GET, "hello", afterResponse: { res in
             XCTAssertEqual(res.status, .ok)
             XCTAssertEqual(res.body.string, "Hello, world!")
         })
+    }
+    
+    
+    func testGodsHome() async throws {
+        let app = Application(.testing)
+        defer { app.shutdown() }
+        try await configure(app)
+        
+        try app.test(.GET, "gods") { res in
+            XCTAssertEqual(res.status, .ok)
+            // Verifica se o Content-Type da resposta é JSON
+            XCTAssertTrue(res.headers.contentType == .json)
+            
+            XCTAssertNoThrow(try res.content.decode([Entity].self))
+            
+            
+        }
+        
     }
 }
